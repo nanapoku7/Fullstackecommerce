81258e82ea2e0a1608e05a91b9b47eddaece33e3
nanapoku7 <nanapokuprempeh38@gmail.com>
2024-12-04 12:32:41 +0000

    Products Router and Controllers

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,20 +1,21 @@
 import { Router } from 'express';
+import {
+     listProducts,
+      getProductById, 
+      createProduct, 
+      updateProduct,
+      deleteProduct, 
+} from './productsController';
 
-// products endpoints
+
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('The List of products 123');
-});
+router.get('/', listProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+   
 
-router.get('/:id', (req, res) => {
-    console.log(req.params)
-    res.send('A product');
-});
-
-router.post('/', (req, res) => {
-    res.send('New product created');
-    
-});
 
 export default router;
